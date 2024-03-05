38e8208b3c1ad430b39fb4ea2e4eeecb169d0b17
ajofracio0723 <abejayofracio@thelewiscollege.edu.ph>
2024-03-05 20:18:30 +0800

    okay fine

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/ticket', [TicketController::class, 'showForm'])->name('ticket.form');
-Route::post('/ticket', [TicketController::class, 'submitForm'])->name('ticket.submit');
+Route::get('/', [TicketController::class, 'showForm'])->name('ticket.form');
+Route::post('/', [TicketController::class, 'submitForm'])->name('ticket.submit');
